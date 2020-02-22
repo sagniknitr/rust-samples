cf86c9ed67deeb4a3b72397612eac5a5feb14c52
sagniknitr <basusagnik1995@gmail.com>
2020-02-22 23:59:01 +0530

    Add cpu affinity code

--- threads/main.rs
+++ threads/main.rs
@@ -12,6 +12,26 @@ fn sample() {
     handle.join();
 }
 
+
+fn cpu_affinity() {
+
+    // Retrieve the IDs of all active CPU cores.
+    let core_ids = core_affinity::get_core_ids().unwrap();
+ 
+// Create a thread for each active CPU core.
+    let handles = core_ids.into_iter().map(|id| {
+    thread::spawn(move || {
+        // Pin this thread to a single CPU core.
+        core_affinity::set_for_current(id);
+        // Do more work after this.
+    })
+    }).collect::<Vec<_>>();
+ 
+    for handle in handles.into_iter() {
+        handle.join().unwrap();
+    }
+
+}
 fn capture() {
     let v = vec![1, 2, 3];
     let x = 5;
