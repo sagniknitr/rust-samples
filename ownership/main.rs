c44055169a4c62dda5bda86b8a14dbefda1e62b0
sagniknitr <basusagnik1995@gmail.com>
2020-03-26 23:11:30 +0530

    Reference use case

--- ownership/main.rs
+++ ownership/main.rs
@@ -28,6 +28,18 @@ fn sum(vector: Vec<i32>) -> i32 {
     sum
 }
 
+
+// No reference , but much more clutter
+fn calculate_hike(s: Info) -> (Info, usize) {
+    let length = s.len(); 
+
+    (s, length)
+}
+
+fn calculate_hike_eff(s: &Info) -> usize {
+    let length = s.len(); 
+}
+
 fn main() {
     let v = vec![1,2,3];
     let s = sum(v); // check on shallow copy vs move in RUST
